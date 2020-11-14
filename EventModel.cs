c3f9df257c1859a64a749fee1a0d9f47591b8f8b
Gordon <54365930+gorgorgordon@users.noreply.github.com>
2020-11-15 01:49:29 +0800

    Create EventModel.cs

---
+++ EventModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcEvent.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string StartDate { get; set; }
+    }
+}
